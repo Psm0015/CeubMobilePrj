f7dbe1cfb030bbc9b64e6151d599bb52c15812ec
pedro-mota <pedro.mota@df.estudante.senai.br>
2023-10-11 23:49:46 -0300

    App Pronto

--- app/src/main/java/com/example/ceubprj/MainActivity.kt
+++ app/src/main/java/com/example/ceubprj/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.example.ceubprj
 
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.ceubprj.ui.theme.CeubPrjTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,11 +24,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CeubPrjTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    WelcomeScreen()
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "welcome_screen") {
+                    composable("welcome_screen") {
+                        WelcomeScreen(navController)
+                    }
+                    composable("webview_screen") {
+                        WebViewScreen()
+                    }
                 }
             }
         }
@@ -29,20 +39,44 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun WelcomeScreen() {
-    var message by remember { mutableStateOf("Bem vindo(a)!") }
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+fun WelcomeScreen(navController: NavHostController) {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
     ) {
-        Button(onClick = { message = "" }) {
-            Text("Clique Aqui")
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(onClick = { navController.navigate("webview_screen") }) {
+                Text("Entrar")
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(text = "Bem-vindo")
         }
-        Spacer(modifier = Modifier.height(16.dp))
-        Text(text = message)
+    }
+}
+
+@Composable
+fun WebViewScreen() {
+    val url = "https://steamdb.info"
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        AndroidView(
+            factory = { context ->
+                WebView(context).apply {
+                    settings.javaScriptEnabled = true
+                    webViewClient = WebViewClient()
+                    loadUrl(url)
+                }
+            },
+            modifier = Modifier.fillMaxSize()
+        )
     }
 }
 
@@ -50,6 +84,14 @@ fun WelcomeScreen() {
 @Composable
 fun WelcomeScreenPreview() {
     CeubPrjTheme {
-        WelcomeScreen()
+        WelcomeScreen(rememberNavController())
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WebViewScreenPreview() {
+    CeubPrjTheme {
+        WebViewScreen()
     }
 }
